7bfde83d4f18217a3090c650a591542075105818
kohei sano <nlee9337@gmail.com>
2025-11-02 18:00:32 +0900

    Refactor EnemyController to use FixedUpdate for movement and adjust speed; add destruction logic when exceeding boundary

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,11 +9,16 @@ public class EnemyController : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector2 pos = rb.position;
 
-        pos += Vector2.right * 30 * Time.deltaTime;
+        pos += Vector2.right * 2 * Time.deltaTime;
+
+        if (pos.x > 10)
+        {
+            Destroy(gameObject);
+        }
 
         rb.MovePosition(pos);
     }
