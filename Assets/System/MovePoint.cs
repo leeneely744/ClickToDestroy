e383981e5b8349a15e3e3ff7d080d09b606e4ee4
kohei sano <nlee9337@gmail.com>
2025-11-03 11:34:14 +0900

    Add Assembly-CSharp-Editor project to solution, create meta files for Editor and MovePointEditor, and update SampleScene with new MovePoint GameObject and functionality for waypoint visualization.

--- Assets/System/MovePoint.cs
+++ Assets/System/MovePoint.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MovePoint : MonoBehaviour
 {
+    public Vector3[] points;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,19 @@ public class MovePoint : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Vector3 GetMovePointPosition(int index)
+    {
+        return points[index];
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(points[i], 0.5f);
+        }
     }
 }
