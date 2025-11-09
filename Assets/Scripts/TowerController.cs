f728e6992535fe103eeb019580ad78440462f09e
kohei sano <nlee9337@gmail.com>
2025-11-09 17:09:48 +0900

    Enhance tower functionality by adding CircleCollider2D components to BowTower, CannonTower, and MageTower prefabs. Update TowerController script to implement attack logic and manage enemies within range.

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -1,6 +1,55 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TowerController : MonoBehaviour
 {
-    // あとで射程や攻撃処理をここに書く
+    public float range = 3f;
+    public float attackInterval = 1.0f;
+    private float attackTimer = 0f;
+
+    private List<EnemyController> enemiesInRange = new List<EnemyController>();
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (attackTimer >= attackInterval)
+        {
+            attackTimer = 0f;
+
+            if (enemiesInRange.Count > 0)
+            {
+                Attack(enemiesInRange[0]);
+            }
+        }
+    }
+
+    void Attack(EnemyController target)
+    {
+        Debug.Log($"Tower attacking enemy: {target.gameObject.name}");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            EnemyController enemy = col.GetComponent<EnemyController>();
+            if (!enemiesInRange.Contains(enemy))
+            {
+                enemiesInRange.Add(enemy);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            EnemyController enemy = col.GetComponent<EnemyController>();
+            if (enemiesInRange.Contains(enemy))
+            {
+                enemiesInRange.Remove(enemy);
+            }
+        }
+    }
 }
