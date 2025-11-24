ca5db2934345f0ace76b8827a4ad3aeba911541f
kohei sano <nlee9337@gmail.com>
2025-11-24 12:50:31 +0900

    Add tower prefabs and associated scripts
    - Created new prefabs for BowTower, CannonTower, MageTower, and Projectile with necessary components.
    - Added TowerStats asset to manage tower statistics and levels.
    - Implemented TowerStats scriptable object to define tower properties.
    - Updated package manifest to include Simple Folder Icon package.

---
+++ Assets/Scripts/TowerStats.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tower/TowerStats", fileName = "TowerStats")]
+public class TowerStats : ScriptableObject
+{
+    public string towerName;
+    public Sprite towerSprite;
+    public TowerLevel[] levels;
+}
+
+[System.Serializable]
+public class TowerLevel
+{
+    public int level;
+    public int cost;
+    public int sellRefund;
+    public int upgradeCost;
+    public float attackDamage;
+    public float attackInterval;
+    public float range;
+}
