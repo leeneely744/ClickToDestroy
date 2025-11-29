ab1815da0369f426626f92ecd1bea68520415488
kohei sano <nlee9337@gmail.com>
2025-11-29 18:36:19 +0900

    feat(towers): update magicTowerPrefab reference and adjust MagicTower3Controller position

--- Assets/Scripts/Towers/MagicTower3Controller.cs
+++ Assets/Scripts/Towers/MagicTower3Controller.cs
@@ -1,4 +1,14 @@
+using UnityEngine;
+
 public class MagicTower3Controller : TowerController
 {
     protected override int InitialLevelIndex => 2;
+
+    private void Start()
+    {
+        // 他よりも背の高いタワーなので、立ち位置を調整する
+        var pos = transform.position;
+        pos.y *= 1.3f;
+        transform.position = pos;
+    }
 }
