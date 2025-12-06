a515ca33ef0836a925ca6b7b53b19346ac88784e
kohei sano <nlee9337@gmail.com>
2025-12-06 13:27:00 +0900

    feat(tower): adjust position for Guardian Tower 3 to account for height

--- Assets/Scripts/Towers/GuardianTower3Controller.cs
+++ Assets/Scripts/Towers/GuardianTower3Controller.cs
@@ -6,4 +6,13 @@ public class GuardianTower3Controller : GuardianTowerControllerBase
 
     [SerializeField] private GameObject guardianPrefab;
     protected override GameObject GuardianPrefab => guardianPrefab;
+
+    protected override void Start()
+    {
+        base.Start();
+        // 他よりも背の高いタワーなので、立ち位置を調整する
+        var pos = transform.position;
+        pos.y *= 1.1f;
+        transform.position = pos;
+    }
 }
