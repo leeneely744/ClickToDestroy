f386235b33a9b9c59ba4e20e4b7c7990ace51b3c
kohei sano <nlee9337@gmail.com>
2025-12-10 23:25:33 +0900

    feat(tower): update radius for CircleCollider2D in MagicTower prefabs and adjust position for Bow and Magic towers

--- Assets/Scripts/Towers/BowTower3Controller.cs
+++ Assets/Scripts/Towers/BowTower3Controller.cs
@@ -7,7 +7,7 @@ public class BowTower3Controller : TowerController
         base.Start();
         // 他よりも背の高いタワーなので、立ち位置を調整する
         var pos = transform.position;
-        pos.y *= 1.4f;
+        pos.y *= 1.2f;
         transform.position = pos;
     }
 }
--- Assets/Scripts/Towers/MagicTower3Controller.cs
+++ Assets/Scripts/Towers/MagicTower3Controller.cs
@@ -9,7 +9,7 @@ public class MagicTower3Controller : TowerController
         base.Start();
         // 他よりも背の高いタワーなので、立ち位置を調整する
         var pos = transform.position;
-        pos.y *= 1.5f;
+        pos.y *= 1.2f;
         transform.position = pos;
     }
 }
